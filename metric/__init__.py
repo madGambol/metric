4b95bfb72c7f4b87345b8f5bfcfa389217407684
Nick Bargnesi <nbargnesi@selventa.com>
2014-11-14 19:36:33 -0500

    add used_memory function

--- metric/__init__.py
+++ metric/__init__.py
@@ -3,6 +3,7 @@
 
 See https://github.com/southcoast-python/metric for more.
 '''
+import resource
 __author__ = 'Metric library authors'
 __copyright__ = 'Copyright 2014, Metric authors'
 __credits__ = [
@@ -13,4 +14,9 @@ __version__ = '0.1'
 __maintainer__ = 'Nick Bargnesi'
 __email__ = 'nbargnesi@selventa.com'
 __status__ = 'alpha'
+
+
+def used_memory():
+    rusage = resource.getrusage(resource.RUSAGE_SELF)     
+    return rusage.ru_maxrss
 
